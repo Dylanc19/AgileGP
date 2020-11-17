c2635aee7222970b3da83c29623d20484264476a
Dylan <dylan.m.casey@mycit.ie>
2020-11-17 23:53:04 +0000

    Trying a new method

--- HelloPython.py
+++ HelloPython.py
@@ -1,7 +1,7 @@
 import datetime
 print("Hello world!")
 
-print("This is the second test for the job")
+print("Testing the new method")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	

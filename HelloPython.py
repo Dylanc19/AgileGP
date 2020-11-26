a5b5ac10bdd86b05af8048ffe20a6eb10507a88e
Dylanc19 <71798101+Dylanc19@users.noreply.github.com>
2020-11-26 22:49:10 +0000

    Testing github actions

--- HelloPython.py
+++ HelloPython.py
@@ -5,6 +5,7 @@ print("Testing the new method")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
+print("Testing Github actions")
 
 
 	

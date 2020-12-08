58670d155a470a55a363bd841a4a83cfdd4a891b
Dylanc19 <71798101+Dylanc19@users.noreply.github.com>
2020-12-08 14:20:55 +0000

    Update HelloPython.py

--- HelloPython.py
+++ HelloPython.py
@@ -5,6 +5,6 @@ now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
 print("Testing Jenkins")
-print("Testing for demo")
+print("Live Testing for demo")
 
 	
